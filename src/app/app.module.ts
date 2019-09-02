ec7e21852818c712cbdd5d1b5e5e0d8bcf8f59d5
Akrem <pgtissot15@gmail.com>
2019-09-02 16:48:04 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { UiModule } from './ui/ui.module';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    NgModule,
+    UiModule,
+    LoginModule
+     ],
   providers: [],
   bootstrap: [AppComponent]
 })
