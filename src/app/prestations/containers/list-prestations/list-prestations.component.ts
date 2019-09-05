6bdfe36920ef8169f4e4856d9ef93df711e9412c
Akrem <pgtissot15@gmail.com>
2019-09-05 08:59:33 +0200

    directive

--- src/app/prestations/containers/list-prestations/list-prestations.component.ts
+++ src/app/prestations/containers/list-prestations/list-prestations.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { PrestationsService } from '../../services/prestations.service';
 import { Prestation } from 'src/app/shared/models/prestation';
-import { State } from 'src/app/shared/enums/state.enum';
 @Component({
  selector: 'app-list-prestations',
  templateUrl: './list-prestations.component.html',
  styleUrls: ['./list-prestations.component.scss']
 })
-export class ListPrestationsComponent implements OnInit {
- public states = State;
- public collection: Prestation[];
+export class ListPrestationsComponent implements OnInit {  public collection: Prestation[];
  public headers = [
    'Type',
    'Client',
@@ -23,10 +20,5 @@ export class ListPrestationsComponent implements OnInit {
    private prestationsService: PrestationsService
  ) { }  ngOnInit() {
    this.collection = this.prestationsService.collection;
-  }
-
-  public changeState(arg, item) {
-    // console.log(arg.target.value);
-    this.prestationsService.update(item, arg.target.value);
   }
 }
